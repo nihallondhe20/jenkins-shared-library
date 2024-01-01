7ba5aba047a63826e54a1b49df2c68e3935d0c27
Nihal Londhe <nihallondhe20@gmail.com>
2024-01-01 14:24:52 +0530

    t1

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,3 +1,3 @@
 def call(){
-    sh "timestamp=$(date +%Y%m%d%H%M%S) | sudo docker tag node:latest nihalsdocker/nodejs:$timestamp"
+    sh 'timestamp=$(date +%Y%m%d%H%M%S)sudo docker tag node:latest nihalsdocker/nodejs:$version'
 }   
