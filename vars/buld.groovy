d358e9bd1f6496bd9fb583334638b58a4ba424f9
Nihal Londhe <nihallondhe20@gmail.com>
2024-01-01 13:22:58 +0530

    build

--- vars/buld.groovy
+++ vars/buld.groovy
@@ -1,3 +1,3 @@
 def call(){
-    sh 'sudo docker build -t nodejs ./simple-node/ '
+    sh 'sudo docker build -t nodejs ./simple-node/ | sudo timestamp=$(date +%Y%m%d%H%M%S) |sudo  tag=$image:$timestamp'
 }
